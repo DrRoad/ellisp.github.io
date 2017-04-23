51b491f488bfdecfa63add74f48945b7496f2452
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-04-23 15:22:35 +1200

    working version of census cartogram shiny app

--- _working/0094-cartograms/server.R
+++ _working/0094-cartograms/server.R
@@ -1,6 +1,10 @@
 library(shiny)
 library(leaflet)
 
+camel_to_english <- function(camelCase){
+   return(gsub("([a-z])([A-Z])", "\\1 \\L\\2", camelCase, perl = TRUE))
+}
+
 load("comb_data.rda")
 load("reg_cart_simpl.rda")
 
@@ -9,12 +13,14 @@ shinyServer(function(input, output) {
    var <- reactive({
       var_name <- paste0("Prop", input$variable, "2013")
       value <- comb_data[ , var_name]
+      print(range(value))
       return(value)
    })
    
    the_data <- reactive({
       tmp <- reg_cart_simpl
       tmp@data[ ,"value"] <- var()
+      tmp@data[ , "label"] <-paste0(tmp@data$Name, " ", round(tmp@data$value * 100, 1), "%")
       return(tmp)
    })   
    
@@ -22,16 +28,34 @@ shinyServer(function(input, output) {
       return(input$colour_order == "Reverse")
    })
    
-   
-   map <- reactive({reg_cart_simpl %>%
+   map <- reactive({
+      the_data() %>%
       leaflet() %>%
-      addPolygons(data = the_data(),
-         color = "#444444", weight = 1,
+      addPolygons(color = "#444444", weight = 1,
          fillOpacity = 1,
-         fillColor = ~colorQuantile(input$colour_scheme, value, reverse = colour_reverse())(value),
-         highlightOptions = highlightOptions(color = "steelblue", weight = 2,
-                                             bringToFront = TRUE))})
+         fillColor = ~colorNumeric(input$colour_scheme, range(var()), reverse = colour_reverse())(value),
+         label = ~label,
+         highlightOptions = highlightOptions(color = "black", weight = 2,
+                                             bringToFront = TRUE)) %>%
+         addLegend(pal = colorNumeric(input$colour_scheme, range(var()), reverse = colour_reverse()), 
+                   values = range(var()), bins = 5, opacity = 1, 
+                   title = "Percentage",
+                   labFormat = labelFormat(
+                      transform = function(x){x * 100}, 
+                      suffix = "%")
+         )
+      }) 
    output$myMap <- renderLeaflet(map())
    
+   the_title <- reactive({
+      tmp <- paste("Proportion", tolower(camel_to_english(input$variable)), "in 2013 Census")
+      tmp <- gsub("_", " ", tmp)
+      return(tmp)
+   })
+   
+   output$the_title <- renderText(the_title())
+
+
+      
    
 })
--- _working/0094-cartograms/ui.R
+++ _working/0094-cartograms/ui.R
@@ -6,22 +6,30 @@ load("variables.rda")
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Stuff"),
+  titlePanel("New Zealand on census night 2013, at a glance"),
   
   sidebarLayout(
     sidebarPanel(
-       selectInput("variable", "Proportion of population - variable for colour", choices = variables),
+       selectInput("variable", "Variable for colour", choices = variables, selected = "NoReligion"),
+       p("...as a proportion of all dwellings, households, or individuals (whichever is relevant)."),
+       hr(),
        selectInput("colour_scheme", "Colour Scheme",
                    choices = c("Reds", "Blues", "Greys", 
                                "Spectral", "YlOrRd", "RdYlBu", "RdGy"),
                    selected = "YlOrRd"),
        radioButtons("colour_order", "Colour order?",
-                    choices = c("Original", "Reverse"))
+                    choices = c("Original", "Reverse"),
+                    selected = "Original")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       leafletOutput('myMap', width = 600, height = 600)
+       h3(textOutput("the_title")),
+       leafletOutput('myMap', width = 600, height = 600),
+       p("Regions are expanded or shrunk to be proportionate to the usual resident population in 2013."),
+       HTML("<p>Source: Statistics New Zealand Census 2013.  Analysed via the <a href = 'https://github.com/ellisp/nzelect'>
+nzcensus</a> R package.  Map was drawn with <a href='http://scapetoad.choros.ch/'>ScapeToad</a>.  See 
+<a href ='https://ellisp.github.io'>Peter's Stats Stuff</a> for more detail.")
     )
   )
 ))
