395e52fe64eee66e512eb8afb60d881d5512fe85
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-08-05 10:58:57 +1200

    source code for post and final working for weighted gini post

--- _working/0106-statsnz-gini.R
+++ _working/0106-statsnz-gini.R
@@ -2,71 +2,10 @@ library(tidyverse)
 library(acid)
 library(microbenchmark)
 library(forcats)
+# Peter's Stats Stuff miscellaneous stuff package, install with: 
+devtools::install_github("ellisp/pssmisc-r-package/pkg")
+library(pssmisc) 
 
-
-
-# John and Draper's modulus transformation
-modulus_trans <- function(lambda){
-  trans_new("modulus",
-            transform = function(y){
-              if(lambda != 0){
-                yt <- sign(y) * (((abs(y) + 1) ^ lambda - 1) / lambda)
-              } else {
-                yt = sign(y) * (log(abs(y) + 1))
-              }
-              return(yt)
-            },
-            inverse = function(yt){
-              if(lambda != 0){
-                y <- ((abs(yt) * lambda + 1)  ^ (1 / lambda) - 1) * sign(yt)
-              } else {
-                y <- (exp(abs(yt)) - 1) * sign(yt)
-                
-              }
-              return(y)
-            }
-  )
-}
-
-
-.mod_transform <- function(y, lambda){
-  if(lambda != 0){
-    yt <- sign(y) * (((abs(y) + 1) ^ lambda - 1) / lambda)
-  } else {
-    yt = sign(y) * (log(abs(y) + 1))
-  }
-  return(yt)
-}
-
-
-.mod_inverse <- function(yt, lambda){
-  if(lambda != 0){
-    y <- ((abs(yt) * lambda + 1)  ^ (1 / lambda) - 1) * sign(yt)
-  } else {
-    y <- (exp(abs(yt)) - 1) * sign(yt)
-    
-  }
-  return(y)
-}
-
-prettify <- function(breaks){
-  # round numbers, more aggressively the larger they are
-  digits <- -floor(log10(abs(breaks))) + 1
-  digits[breaks == 0] <- 0
-  return(round(breaks, digits = digits))
-}
-
-mod_breaks <- function(lambda, n = 8, prettify = TRUE){
-  function(x){
-    breaks <- .mod_transform(x, lambda) %>%
-      pretty(n = n) %>%
-      .mod_inverse(lambda)
-    if(prettify){
-      breaks <- prettify(breaks)
-    }
-    return(breaks)
-  }
-}
 
 
 StatsGini <- function(x, w = rep(1, length(x))){
@@ -130,15 +69,11 @@ gen_incomes <- function(n, mu = 30000,
 
 set.seed(123)
 N <- 100000
-population <- data.frame(income = gen_incomes(N), sigma_alpha = 1, d_alpha = 10, d_beta = 150,
+population <- data.frame(income = gen_incomes(N), sigma_alpha = 1, d_alpha = 10, d_beta = 15,
                          sigma_beta = 2,
-                         prob = rbeta(n, 2, 2))
+                         prob = rbeta(N, 2, 2))
 
-# best value of lambda for transforming? Turns out to be very low, so almost like doing a log transform
-forecast::BoxCox.lambda(sample(abs(population$income), 1000))
 
-# breaks look dreadful, use the approach at http://ellisp.github.io/blog/2015/09/07/transforming-breaks-in-a-scale
-# instead.
 svg("../img/0106-density.svg", 7, 4)
 population %>%
   slice(1:1000) %>%
@@ -146,12 +81,34 @@ population %>%
   scale_x_continuous("Annual income (modulus transform with power of 0.02)",
                      label = dollar, 
                      trans = modulus_trans(0.02),
-                     breaks = mod_breaks(lambda = 0.02)) + 
+                     breaks = modulus_breaks(lambda = 0.02)) + 
   geom_density(fill = "grey", alpha = 0.2)  +
   geom_rug() +
   ggtitle("Density of simulated annual incomes",
           "First 1,000 values")
 dev.off()
+
+svg("../img/0106-lorenz.svg", 7, 5)
+population %>%
+  # thin down to just a sample of 10,000 to save on vector graphic file size:
+  sample_n(10000) %>%
+  arrange(income) %>%
+  mutate(cum_prop_inc = cumsum(income) / sum(income),
+         seq = 1:n() / n()) %>%
+  ggplot(aes(x = seq, y = cum_prop_inc)) +
+  geom_line() +
+  geom_ribbon(aes(ymax = seq, ymin = cum_prop_inc), fill = "steelblue", alpha = 0.2) +
+  geom_abline(intercept = 0, slope = 1, colour = "steelblue") +
+  labs(x = "Cumulative proportion of population, from poorest to richest",
+       y = "Cumulative proportion of income") +
+  annotate("text", x = c(0.6, 0.95), y = c(0.4, 0.05), label = c("A", "B"), size = 7) +
+  annotate("text", x = 0.5, y = 0.6, label = "Line of equality", angle = 45) +
+  annotate("text", x = 0.85, y = 0.15, label = "Actual distribution", angle = 45) +
+  coord_equal() +
+  ggtitle("Lorenz curve with simulated income data",
+          paste("Gini coefficient is", round(gini(population$income)$Gini, 2)))
+dev.off()  
+
 
 small_sample <- population[1:100, ]
 weighted.gini(small_sample$income, w = 1 / small_sample$prob)$Gini
@@ -177,6 +134,7 @@ reps <- 100
 # sample sizes, chosen to resemble realistic survey sizes:
 ns <- c(30, 100, 500, 1000, 3000, 10000)
 
+# matrix of zeros that we'll replace with the estimates of Gini
 res <- matrix(0, nrow = reps, ncol = length(ns))
 
 # obviously this next bit could be made much more efficient by parallel processing,
@@ -205,8 +163,10 @@ res_df %>%
   coord_cartesian(xlim = c(0.6, 1)) +
   labs(x = "Estimated weighted Gini") +
   ggtitle("Sampling distribution of weighted Gini point estimate from a complex survey",
-          paste0("Various sample sizes; true value is ", round(weighted.gini(the_sample$income)$Gini, 3)))
+          paste0("Various sample sizes; true value is ", round(weighted.gini(population$income)$Gini, 2)))
 
 dev.off()
 
 # note might be a bug in dplyr, format doesn't work in the pipeline?
+
+convert_pngs("0106")
