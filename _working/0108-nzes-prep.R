9245960958acb28018ffaba926fe77bcd8f23f88
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-08-19 20:13:42 +1200

    fix problem where factors lost all their ordering of levels for the NZES cross tab app.

--- _working/0108-nzes-prep.R
+++ _working/0108-nzes-prep.R
@@ -1,5 +1,6 @@
 # This script is prep for the shiny app in the ./_working/0108/ folder
-#
+# See previous blog posts for sourcing the original data
+# App is deployed to https://ellisp.shinyapps.io/nzes2014_x_by_party/ 
 
 library(foreign)
 library(tidyverse)
@@ -8,20 +9,24 @@ library(DT)
 library(survey)
 library(rsconnect)
 
+# Import the New Zealand Election Study data:
 nzes_orig <- read.spss("NZES2014GeneralReleaseApril16.sav", 
                        to.data.frame = TRUE, trim.factor.names = TRUE, reencode = FALSE)
 
+# full names for all the questions
 varlab <- cbind(attributes(nzes_orig)$variable.labels)
 
+# subset which variables we want
 vars <- varlab[c(10:22, 94:99, 178:212, 215, 261, 270:288, 308:324, 331, 341:343,
                  351:358, 362:381, 385:403), ] 
 vars <- gsub("M.ori", "Māori", vars)
 
-
+# tidy up the data
 nzes <- nzes_orig %>%
   map_df(function(x){
-    if(!is.numeric(x)){
-      gsub("M.ori", "Māori", x)
+    if(is.factor(x)){
+      levels(x) <-  gsub("M.ori", "Māori", levels(x))
+      x
     } else {
       x
     }}) %>%
@@ -32,9 +37,7 @@ nzes <- nzes_orig %>%
          # This magic constant, 3140417 was the size of the electoral roll at the time of election;
          # see http://www.electionresults.govt.nz/electionresults_2014/e9/html/e9_part9_1.html
          dwtfin = dwtfin * 3140.417 / sum(dwtfin) ) 
-  
-  
-nzes <- nzes[ ,c(names(vars), "partyvote", "dwtfin")]
+  nzes <- nzes[ ,c(names(vars), "partyvote", "dwtfin")]
 
 party_numbers <- nzes %>%
   group_by(partyvote) %>%
